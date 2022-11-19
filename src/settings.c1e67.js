434058995c2975c292bcc8a5c0978ec52db5a4e0
Ketan Shukla <ketan.shukla@gameium.com>
2022-11-19 11:57:04 +0530

    added game files

---
+++ src/settings.c1e67.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scenes/game.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "4210e",
+        main: "8aec4"
+    }
+};
